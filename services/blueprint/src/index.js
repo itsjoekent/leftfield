a9b4f20c7d4ac82d7397cdc09f34f5bc61c9a9d9
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2021-07-21 14:46:15 -0400

    adding blueprint?

--- services/blueprint/src/index.js
+++ services/blueprint/src/index.js
@@ -1,8 +1,11 @@
+const path = require('path');
+
 if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: path.resolve(process.cwd(), '.env.local') });
   require('../../../environment/local-development');
 }
 
-const logger = require('./utils/logger');
+const { logger } = require('pkg.technician');
 
 logger.debug('Starting up...');
 
--- services/blueprint/src/routes/index.js
+++ services/blueprint/src/routes/index.js
@@ -1,19 +1,37 @@
 const NODE_ENV = process.env.NODE_ENV;
 const PORT = process.env.BLUEPRINT_API_PORT;
 
+const { randomBytes } = require('crypto');
+
 const express = require('express');
 const pinoHttp = require('pino-http');
 
-const logger = require('../utils/logger');
+const { logger } = require('pkg.technician');
 
-const httpLogger = pinoHttp({ logger });
+const httpLogger = pinoHttp({
+  logger,
+  genReqId: (req) => req.__id,
+});
 
 const app = express();
+
+app.use(async (req, res, next) => {
+  const buffer = randomBytes(32);
+  req.__id = buffer.toString('hex');
+  next();
+});
+
 app.use(httpLogger);
 
 app.get('/', (req, res) => {
   res.send('hello');
 });
+
+const router = express.Router();
+
+require('./login')(router);
+
+app.use('/api', router);
 
 app.listen(PORT, () => {
   logger.info(`Listening on PORT:${PORT} in NODE_ENV:${NODE_ENV}`);
