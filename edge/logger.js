57889ca9c6460eccb2c927b839052f600bc53158
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2021-08-28 01:54:48 -0400

    Fixing edge logger

--- edge/logger.js
+++ edge/logger.js
@@ -3,7 +3,7 @@ const NODE_ENV = process.env.NODE_ENV;
 const pino = require('pino');
 
 const logger = pino({
-  prettyPrint: NODE_ENV !== 'production',
+  prettyPrint: NODE_ENV === 'development',
 });
 
 module.exports = logger;
