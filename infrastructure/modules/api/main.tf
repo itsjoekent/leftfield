f7d32d685a46d887f61ea6ddcab931cb7c290458
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2021-09-10 13:25:24 -0400

    Minor cleanup

--- infrastructure/modules/api/main.tf
+++ infrastructure/modules/api/main.tf
@@ -170,26 +170,3 @@ module "task_ssl" {
     aws = aws
   }
 }
-
-# module "elastic_container" {
-#   source = "./elastic-container"
-#   config = local.config
-#   region = var.region
-#
-#   cache_redis        = module.cache.redis
-#   cache_user         = module.cache.user
-#   http_target_group  = module.load_balancer.http_target_group
-#   https_target_group = module.load_balancer.https_target_group
-#   image_repository   = var.image_repository
-#   private_subnets    = module.network.private_subnets
-#   vpc                = module.network.vpc
-#
-#   providers = {
-#     aws = aws
-#   }
-#
-#   depends_on = [
-#     module.load_balancer.http_load_balancer_listener,
-#     module.load_balancer.https_load_balancer_listener,
-#   ]
-# }
